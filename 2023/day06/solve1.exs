3739d20eb183c4527ebc4607d314ca43c74e7b5b
Naveed Massjouni <naveed@vt.edu>
2023-12-06 00:51:32 -0500

    day06

---
+++ 2023/day06/solve1.exs
@@ -0,0 +1,32 @@
+defmodule Main do
+
+  def main() do
+    "input-large.txt"
+    |> parse_input()
+    |> Enum.map(fn race -> ways_to_win(race) end)
+    |> Enum.product()
+  end
+
+  def ways_to_win({t, record_d}) do
+    1..t |> Enum.count(fn speed -> speed * (t - speed) > record_d end)
+  end
+
+  def parse_input(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(":")
+      |> List.last()
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.zip()
+  end
+end
+
+Main.main() |> IO.puts()
+
+# 288 - input-small.txt answer
+# 393120 - input-large.txt answer
---
+++ 2023/day06/solve2.exs
@@ -0,0 +1,24 @@
+defmodule Main do
+
+  def main() do
+    "input-large.txt" |> parse_input() |> ways_to_win()
+  end
+
+  def ways_to_win([t, record_d]) do
+    1..t |> Enum.count(fn speed -> speed * (t - speed) > record_d end)
+  end
+
+  def parse_input(path) do
+    path
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      Regex.scan(~r/\d+/, line) |> List.flatten() |> Enum.join() |> String.to_integer()
+    end)
+  end
+end
+
+Main.main() |> IO.puts()
+
+# 71503 - input-small.txt answer
+# 36872656 - input-large.txt answer
