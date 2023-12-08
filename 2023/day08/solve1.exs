c142d0328dc8d65b1b74fa7af6d2672abf57ce85
Naveed Massjouni <naveed@vt.edu>
2023-12-08 01:12:51 -0500

    cleanup

--- 2023/day08/solve1.exs
+++ 2023/day08/solve1.exs
@@ -1,29 +1,22 @@
 defmodule Main do
-
   def main() do
-    #{graph, steps} = "input-small.txt" |> parse_input
     {graph, steps} = "input-large.txt" |> parse_input
-
     search(graph, steps, steps, "AAA", 1)
   end
 
-  #def search(graph, steps, _, _, 9), do: "oops"
+  def search(_graph, _steps, _orig_steps, "ZZZ", cnt), do: cnt - 1
 
-  def search(graph, steps, orig_steps, "ZZZ", cnt), do: cnt - 1
+  def search(graph, [], orig_steps, node, cnt) do
+    search(graph, orig_steps, orig_steps, node, cnt)
+  end
 
-  def search(graph, [], orig_steps, node, cnt), do: search(graph, orig_steps, orig_steps, node, cnt)
-
-  def search(graph, [ dir | steps], orig_steps, node, cnt) do
+  def search(graph, [dir | steps], orig_steps, node, cnt) do
     next = graph[node][dir]
-    search(graph, steps, orig_steps, next, cnt+1)
+    search(graph, steps, orig_steps, next, cnt + 1)
   end
 
   def parse_input(path) do
-    lines =
-      path
-      |> File.read!()
-      |> String.trim()
-      |> String.split("\n", trim: true)
+    lines = path |> File.read!() |> String.split("\n", trim: true)
 
     [steps | lines] = lines
     steps = String.codepoints(steps)
@@ -31,19 +24,17 @@ defmodule Main do
     graph =
       lines
       |> Enum.map(fn line ->
-        Regex.run(~r/(...) = \((...), (...)\)/, line) |> tl
+        Regex.run(~r/(...) = \((...), (...)\)/, line) |> tl()
       end)
-      |> Enum.reduce(%{}, fn [a, b, c], acc ->
-        Map.put(acc, a, %{"L" => b, "R" => c})
+      |> Enum.reduce(%{}, fn [node, l, r], acc ->
+        Map.put(acc, node, %{"L" => l, "R" => r})
       end)
 
     {graph, steps}
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
-
 end
 
-Main.main() |> IO.inspect()
+Main.main() |> IO.puts()
+
+# 6 - input-small.txt answer
+# 20093 - input-large.txt answer
