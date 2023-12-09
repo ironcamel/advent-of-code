088f88932a7072978bb61bdbfdccb15656ee4424
Naveed Massjouni <naveed@vt.edu>
2023-12-09 00:55:09 -0500

    cleanup

--- 2023/day09/solve1.exs
+++ 2023/day09/solve1.exs
@@ -1,21 +1,12 @@
 defmodule Main do
-
   def main() do
-    #rows = "input-small.txt" |> parse_input
-    rows = "input-large.txt" |> parse_input
-
-    #rows = [List.last(rows)]
-    #rows = [Enum.at(rows, 1)]
-
-    rows
+    "input-large.txt"
+    |> parse_input()
     |> Enum.map(fn row -> process_row(row) end)
     |> Enum.sum()
-
   end
 
-  def process_row(row) do
-    process_row(row, [row])
-  end
+  def process_row(row), do: process_row(row, [row])
 
   def process_row(row, acc) do
     if Enum.all?(row, fn x -> x == 0 end) do
@@ -28,7 +19,7 @@ defmodule Main do
         |> Enum.chunk_every(2, 1, :discard)
         |> Enum.map(fn [x, y] -> y - x end)
 
-      process_row(new_row, [ new_row | acc ])
+      process_row(new_row, [new_row | acc])
     end
   end
 
@@ -37,19 +28,11 @@ defmodule Main do
     |> File.read!()
     |> String.trim()
     |> String.split("\n", trim: true)
-    |> Enum.map(fn line ->
-      line
-      |> String.split()
-      |> Enum.map(&String.to_integer/1)
-    end)
+    |> Enum.map(fn line -> line |> String.split() |> Enum.map(&String.to_integer/1) end)
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
-
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
 
-# 1167518841 is wrong
+# 114 - input-small.txt answer
+# 1479011877 - input-large.txt answer
