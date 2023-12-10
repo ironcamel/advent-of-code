b549ab6241ca973ef0ff476b0b4c9cd002a09fc6
Naveed Massjouni <naveed@vt.edu>
2023-12-10 07:49:22 -0500

    cleanup

--- 2023/day10/solve2.exs
+++ 2023/day10/solve2.exs
@@ -3,60 +3,45 @@ defmodule Main do
     graph = "input-large.txt" |> parse_input
     {start, _} = graph |> Enum.find(fn {key, _val} -> graph[key].type == "S" end)
     visited = dfs(graph, start)
-    process(graph, visited, 0)
+    search_unvisited(graph, visited, 0)
   end
 
-  def process(graph, visited, sum) do
+  def search_unvisited(graph, visited, sum) do
     unvisited_item =
       Enum.find(graph, fn {key, _val} ->
         not MapSet.member?(visited, key) and is_orig(key)
       end)
 
     if unvisited_item do
-      visited2 = dfs2(elem(unvisited_item, 0), graph, visited)
+      {start_key, _} = unvisited_item
+      visited2 = dfs2(graph, start_key, visited)
       newly_visited = MapSet.difference(visited2, visited)
 
+      # If any of the keys are on an edge (i=0 or j=0), then ignore because we must be outside of the loop
       if Enum.any?(newly_visited, fn {i, j} -> i == 0 || j == 0 end) do
-        process(graph, visited2, sum)
+        search_unvisited(graph, visited2, sum)
       else
         cnt = newly_visited |> Enum.count(fn key -> is_orig(key) end)
-        process(graph, visited2, sum + cnt)
+        search_unvisited(graph, visited2, sum + cnt)
       end
     else
       sum
     end
   end
 
+  # We stretched the original graph by a factor of 9. Every 1x1 unit became 3x3.
   def is_orig({i, j}), do: rem(i, 3) == 1 and rem(j, 3) == 1
 
-  def dfs2({i, j} = start, graph, visited) do
-    visited = MapSet.put(visited, start)
-
-    children =
-      [
-        {i + 1, j},
-        {i - 1, j},
-        {i, j + 1},
-        {i, j - 1}
-      ]
-      |> Enum.filter(fn key -> graph[key] && !MapSet.member?(visited, key) end)
-
-    dfs2(graph, children, visited)
-  end
+  def dfs2(graph, key, visited) when not is_list(key), do: dfs2(graph, [key], visited)
 
   def dfs2(_graph, [], visited), do: visited
 
   def dfs2(graph, [key | keys], visited) do
-    {i, j} = key
     visited = MapSet.put(visited, key)
+    {i, j} = key
 
     children =
-      [
-        {i + 1, j},
-        {i - 1, j},
-        {i, j + 1},
-        {i, j - 1}
-      ]
+      [{i + 1, j}, {i - 1, j}, {i, j + 1}, {i, j - 1}]
       |> Enum.filter(fn key -> graph[key] && !MapSet.member?(visited, key) end)
 
     dfs2(graph, children ++ keys, visited)
@@ -78,49 +63,32 @@ defmodule Main do
     dfs(graph, children ++ keys, visited)
   end
 
-  # def n(graph, {i, j} = key), do: n(graph, key, {i - 1, j}, graph[key].type)
-  # def s(graph, {i, j} = key), do: s(graph, key, {i + 1, j}, graph[key].type)
-  # def e(graph, {i, j} = key), do: e(graph, key, {i, j + 1}, graph[key].type)
-  # def w(graph, {i, j} = key), do: w(graph, key, {i, j - 1}, graph[key].type)
+  def check_types(graph, key, next_key, src_types, target_types) do
+    src_type = graph[key].type
+
+    if src_type in src_types && graph[next_key] && graph[next_key].type in target_types,
+       do: next_key,
+       else: nil
+  end
 
   def n(graph, {i, j} = key) do
     next_key = {i - 1, j}
-    type = graph[key].type
-
-    if type in ["S", "|", "J", "L"] && graph[next_key] &&
-         graph[next_key].type in ["|", "7", "F", "S"],
-       do: next_key,
-       else: nil
+    check_types(graph, key, next_key, ["S", "|", "J", "L"], ["|", "7", "F", "S"])
   end
 
   def s(graph, {i, j} = key) do
     next_key = {i + 1, j}
-    type = graph[key].type
-
-    if type in ["S", "|", "F", "7"] && graph[next_key] &&
-         graph[next_key].type in ["|", "J", "L", "S"],
-       do: next_key,
-       else: nil
+    check_types(graph, key, next_key, ["S", "|", "F", "7"], ["|", "J", "L", "S"])
   end
 
   def e(graph, {i, j} = key) do
     next_key = {i, j + 1}
-    type = graph[key].type
-
-    if type in ["S", "L", "F", "-"] && graph[next_key] &&
-         graph[next_key].type in ["7", "J", "-", "S"],
-       do: next_key,
-       else: nil
+    check_types(graph, key, next_key, ["S", "L", "F", "-"], ["7", "J", "-", "S"])
   end
 
   def w(graph, {i, j} = key) do
     next_key = {i, j - 1}
-    type = graph[key].type
-
-    if type in ["S", "J", "7", "-"] && graph[next_key] &&
-         graph[next_key].type in ["F", "L", "-", "S"],
-       do: next_key,
-       else: nil
+    check_types(graph, key, next_key, ["S", "J", "7", "-"], ["F", "L", "-", "S"])
   end
 
   def next_pipes(graph, key) do
