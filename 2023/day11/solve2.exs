75a2af394a4975f1d48dbcb80ed37f078339f7c1
Naveed Massjouni <naveed@vt.edu>
2023-12-11 00:57:10 -0500

    day11 part 2

---
+++ 2023/day11/solve2.exs
@@ -0,0 +1,81 @@
+defmodule Main do
+
+  def main() do
+    {graph, empty_rows, empty_cols} = parse_input("input-large.txt")
+
+    galaxies =
+      graph
+      |> Enum.filter(fn {_key, val} -> val == "#" end)
+      |> Enum.map(fn {key, _c} -> key end)
+      |> Enum.sort()
+
+    for g1 <- galaxies, g2 <- galaxies do
+      {g1, g2}
+    end
+    |> Enum.map(fn pair -> pair |> Tuple.to_list() |> Enum.sort() end)
+    |> Enum.filter(fn [a, b] -> a != b end)
+    |> Enum.uniq()
+    |> Enum.map(fn [{ai, aj}, {bi, bj}] ->
+      x_diff = abs(ai - bi)
+      y_diff = abs(aj - bj)
+      [ai, bi] = Enum.sort([ai, bi])
+      x_range = ai..bi
+      [aj, bj] = Enum.sort([aj, bj])
+      y_range = aj..bj
+      num_rows = empty_rows |> Enum.count(fn i -> i in x_range end)
+      num_cols = empty_cols |> Enum.count(fn j -> j in y_range end)
+      exp_factor = 1_000_000 - 1
+      #exp_factor = 10
+      #exp_factor = 9
+      x_diff + y_diff + (num_rows * exp_factor) + (num_cols * exp_factor)
+    end)
+    |> Enum.sum()
+    #|> length()
+
+  end
+
+  def parse_input(path) do
+
+    lines =
+      path
+      |> File.read!()
+      |> String.trim()
+      |> String.split("\n", trim: true)
+
+    graph =
+      lines
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {line, i} ->
+        #{line |> String.codepoints() |> Enum.with_index(), i}
+        line |> String.codepoints() |> Enum.with_index() |> Enum.map(fn {c, j} -> {i, j, c} end)
+      end)
+      |> Enum.reduce(%{}, fn {i, j, c}, acc -> Map.put(acc, {i, j}, c) end)
+
+    empty_rows =
+      lines
+      |> Enum.with_index()
+      |> Enum.filter(fn {row, _i} -> row =~ ~r/^\.+$/ end)
+      |> Enum.map(fn {_row, i} -> i end)
+
+    empty_cols =
+      lines
+      |> Enum.map(&String.codepoints/1)
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.with_index()
+      |> Enum.filter(fn {col, _j} -> Enum.all?(col, &(&1 == ".")) end)
+      |> Enum.map(fn {_col, j} -> j end)
+
+    {graph, empty_rows, empty_cols}
+  end
+
+  def p(o, opts \\ []) do
+    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
+  end
+
+end
+
+Main.main() |> IO.inspect()
+
+# 82000210 - input-small.txt answer
+# 382979724122 - input-large.txt answer
