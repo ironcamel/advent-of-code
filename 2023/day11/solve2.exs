98be2104ebc71a48d643a42449461a9a9d99dbef
Naveed Massjouni <naveed@vt.edu>
2023-12-11 01:02:15 -0500

    cleanup

--- 2023/day11/solve2.exs
+++ 2023/day11/solve2.exs
@@ -1,5 +1,4 @@
 defmodule Main do
-
   def main() do
     {graph, empty_rows, empty_cols} = parse_input("input-large.txt")
 
@@ -9,44 +8,32 @@ defmodule Main do
       |> Enum.map(fn {key, _c} -> key end)
       |> Enum.sort()
 
-    for g1 <- galaxies, g2 <- galaxies do
-      {g1, g2}
-    end
-    |> Enum.map(fn pair -> pair |> Tuple.to_list() |> Enum.sort() end)
+    (for g1 <- galaxies, g2 <- galaxies, do: [g1, g2])
+    |> Enum.map(fn pair -> Enum.sort(pair) end)
     |> Enum.filter(fn [a, b] -> a != b end)
     |> Enum.uniq()
     |> Enum.map(fn [{ai, aj}, {bi, bj}] ->
-      x_diff = abs(ai - bi)
-      y_diff = abs(aj - bj)
       [ai, bi] = Enum.sort([ai, bi])
+      x_diff = bi - ai
       x_range = ai..bi
       [aj, bj] = Enum.sort([aj, bj])
+      y_diff = bj - aj
       y_range = aj..bj
-      num_rows = empty_rows |> Enum.count(fn i -> i in x_range end)
-      num_cols = empty_cols |> Enum.count(fn j -> j in y_range end)
+      num_rows = Enum.count(empty_rows, fn i -> i in x_range end)
+      num_cols = Enum.count(empty_cols, fn j -> j in y_range end)
       exp_factor = 1_000_000 - 1
-      #exp_factor = 10
-      #exp_factor = 9
       x_diff + y_diff + (num_rows * exp_factor) + (num_cols * exp_factor)
     end)
     |> Enum.sum()
-    #|> length()
-
   end
 
   def parse_input(path) do
-
-    lines =
-      path
-      |> File.read!()
-      |> String.trim()
-      |> String.split("\n", trim: true)
+    lines = path |> File.read!() |> String.trim() |> String.split("\n", trim: true)
 
     graph =
       lines
       |> Enum.with_index()
       |> Enum.flat_map(fn {line, i} ->
-        #{line |> String.codepoints() |> Enum.with_index(), i}
         line |> String.codepoints() |> Enum.with_index() |> Enum.map(fn {c, j} -> {i, j, c} end)
       end)
       |> Enum.reduce(%{}, fn {i, j, c}, acc -> Map.put(acc, {i, j}, c) end)
@@ -68,11 +55,6 @@ defmodule Main do
 
     {graph, empty_rows, empty_cols}
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
-
 end
 
 Main.main() |> IO.inspect()
