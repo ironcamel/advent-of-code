63f0e89d94017a8f49ec7e1ecf9363f323880bc3
Naveed Massjouni <naveed@vt.edu>
2023-12-12 23:53:26 -0500

    cleanup

--- 2023/day12/solve1.exs
+++ 2023/day12/solve1.exs
@@ -1,65 +1,49 @@
 defmodule Main do
-
   def main() do
-    rows = "input-large.txt" |> parse_input
-    #rows = "input-small.txt" |> parse_input
-    #rows = "foo.txt" |> parse_input
-
-    #process_row(hd rows)
-    #rows |> Enum.map(fn {springs, _} -> score(springs) end)
-    rows
-    |> Enum.map(fn row -> process_row(row) end)
+    "input-large.txt"
+    |> parse_input()
+    |> Enum.map(&process_row(&1))
     |> Enum.sum()
-
   end
 
   def process_row({springs, counts}) do
     total = Enum.sum(counts)
+    numq = Enum.count(springs, fn c -> c == "?" end)
     numh = Enum.count(springs, fn c -> c == "#" end)
     need = total - numh
-    numq = Enum.count(springs, fn c -> c == "?" end)
-    #dbg()
+
     0..(2 ** numq - 1)
     |> Enum.count(fn i ->
-      b = Integer.to_string(i, 2)
-      b = String.pad_leading(b, numq, "0") |> String.codepoints
-      valid?(springs, b, need, counts)
+      bits = Integer.to_string(i, 2) |> String.pad_leading(numq, "0") |> String.codepoints()
+      valid?(springs, bits, counts, need)
     end)
   end
 
-  def fill_springs(springs, b), do: fill_springs(springs, b, [])
+  def fill_springs(springs, bits, res \\ [])
 
   def fill_springs([], _, res), do: res
 
-  def fill_springs([c | springs], [], res) do
-    fill_springs(springs, [], res ++ [c])
+  def fill_springs(["?" | springs], [bit | bits], res) do
+    c = if bit == "1", do: "#", else: "."
+    fill_springs(springs, bits, res ++ [c])
   end
 
-  def fill_springs(["?" | springs], [b0 | b], res) do
-    c = if b0 == "1", do: "#", else: "."
-    fill_springs(springs, b, res ++ [c])
+  def fill_springs([c | springs], bits, res) do
+    fill_springs(springs, bits, res ++ [c])
   end
 
-  def fill_springs([c | springs], b, res) do
-    fill_springs(springs, b, res ++ [c])
-  end
-
-  def valid?(springs, b, need, counts) do
-    if Enum.count(b, fn c -> c == "1" end) == need do
-      filled = fill_springs(springs, b)
-      #dbg
-      score(filled) == counts
+  def valid?(springs, bits, counts, need) do
+    if Enum.count(bits, fn c -> c == "1" end) == need do
+      springs |> fill_springs(bits) |> count_springs() == counts
     else
       false
     end
   end
 
-  def score(springs) do
-    s = springs |> Enum.join()
-    Regex.replace(~r/\.\.+/, s, ".")
-    |> String.split(".", trim: true)
+  def count_springs(springs) do
+    Regex.scan(~r/#+/, Enum.join(springs))
+    |> List.flatten()
     |> Enum.map(&String.length/1)
-    #|> dbg
   end
 
   def parse_input(path) do
@@ -68,19 +52,15 @@ defmodule Main do
     |> String.trim()
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
-      [a, b] = String.split(line)
-      a = String.codepoints(a)
-      b = b |> String.split(",") |> Enum.map(&String.to_integer/1)
-      {a, b}
+      [springs, counts] = String.split(line)
+      springs = String.codepoints(springs)
+      counts = counts |> String.split(",") |> Enum.map(&String.to_integer/1)
+      {springs, counts}
     end)
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
-
 end
 
-Main.main() |> IO.inspect()
+Main.main() |> IO.puts()
 
+# 21 input-small.txt answer
 # 7221 - input-large.txt answer
