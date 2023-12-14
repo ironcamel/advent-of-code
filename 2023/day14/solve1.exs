0345e97fcb610742172b5fbcb2aadd2fec0bc8ab
Naveed Massjouni <naveed@vt.edu>
2023-12-14 03:14:13 -0500

    cleanup

--- 2023/day14/solve1.exs
+++ 2023/day14/solve1.exs
@@ -1,23 +1,10 @@
 defmodule Main do
-
   def main() do
-    graph = "input-large.txt" |> parse_input
-    #graph = "input-small.txt" |> parse_input
-    #graph = "foo.txt" |> parse_input
-    #
-
-    #num_rows = graph |> Enum.map(fn {i, j} -> i end) |> Enum.max() |> then(&(&1 + 1))
-    max_i = graph |> Enum.map(fn {{i, _j}, _} -> i end) |> Enum.max()
-    num_rows = max_i + 1
+    {graph, num_rows} = parse_input("input-large.txt")
 
     tilt(graph)
-    #|> print(max_i)
-    |> Enum.map(fn {{i, _j}, val} ->
-      case val do
-        "O" -> num_rows - i
-        _ -> 0
-      end
-    end)
+    |> Enum.filter(fn {_key, val} -> val == "O" end)
+    |> Enum.map(fn {{i, _j}, _val} -> num_rows - i end)
     |> Enum.sum()
   end
 
@@ -28,53 +15,42 @@ defmodule Main do
       |> Enum.reduce(graph, fn {key, val}, acc ->
         tilt(acc, key, val)
       end)
-    if new_graph == graph do
-      graph
-    else
-      tilt(new_graph)
-    end
+
+    if new_graph == graph, do: graph, else: tilt(new_graph)
   end
 
   def tilt(graph, {i, j}, val) do
     target = graph[{i - 1, j}]
+
     if val == "O" and target == "." do
-      graph
-      |> Map.put({i - 1, j}, "O")
-      |> Map.put({i, j}, ".")
+      graph |> Map.put({i - 1, j}, "O") |> Map.put({i, j}, ".")
     else
       graph
     end
   end
 
   def parse_input(path) do
-    path
-    |> File.read!()
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn line -> String.codepoints(line) end)
-    |> Enum.with_index()
-    |> Enum.flat_map(fn {line, i} ->
-      line |> Enum.with_index() |> Enum.map(fn {val, j} -> {i, j, val} end)
-    end)
-    |> Enum.reduce(%{}, fn {i, j, val}, acc ->
-      Map.put(acc, {i, j}, val)
-    end)
-  end
+    lines =
+      path
+      |> File.read!()
+      |> String.split("\n", trim: true)
 
-  def print(graph, max_i) do
-    0..max_i
-    |> Enum.each(fn i ->
-      graph |> Enum.filter(fn {{i2, _j}, _} -> i2 == i end) |> Enum.sort()
-      |> Enum.map(fn {_, val} -> val end) |> Enum.join() |> IO.puts
-    end)
-    graph
-  end
+    graph =
+      lines
+      |> Enum.map(&String.codepoints/1)
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {line, i} ->
+        line |> Enum.with_index() |> Enum.map(fn {val, j} -> {i, j, val} end)
+      end)
+      |> Enum.reduce(%{}, fn {i, j, val}, acc ->
+        Map.put(acc, {i, j}, val)
+      end)
 
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
+    {graph, length(lines)}
   end
-
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
 
+# 136 - input-small.txt answer
 # 108614 - input-large.txt answer
