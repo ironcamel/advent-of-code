40b6090e61b98f07824049f581fdbdf0d93b9a06
Naveed Massjouni <naveed@vt.edu>
2023-12-15 01:57:24 -0500

    cleanup

--- 2023/day15/solve1.exs
+++ 2023/day15/solve1.exs
@@ -1,21 +1,13 @@
 defmodule Main do
-
   def main() do
-    s = "input-large.txt" |> parse_input
-    #s = "input-small.txt" |> parse_input
-    s |> Enum.map(&hash/1) |> Enum.sum()
+    "input-large.txt" |> parse_input() |> Enum.map(&hash/1) |> Enum.sum()
   end
 
   def hash(s) do
     s |> String.to_charlist() |> Enum.reduce(0, fn c, acc -> rem((acc + c) * 17, 256) end)
   end
 
-  def parse_input(path) do
-    path
-    |> File.read!()
-    |> String.trim()
-    |> String.split(",")
-  end
+  def parse_input(path), do: path |> File.read!() |> String.trim() |> String.split(",")
 end
 
 Main.main() |> IO.puts()
