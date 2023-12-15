878874b5d1742ece9573947b462a53c38baf43a4
Naveed Massjouni <naveed@vt.edu>
2023-12-15 02:11:38 -0500

    cleanup

--- 2023/day15/solve2.exs
+++ 2023/day15/solve2.exs
@@ -1,25 +1,16 @@
 defmodule Main do
   def main() do
-    map =
-      "input-small.txt"
-      #"input-large.txt"
-      |> parse_input()
-      |> Enum.reduce(%{}, fn cmd, acc ->
-        #dbg
-        do_cmd(cmd, acc)
-        #|> dbg
-      end)
-
-    map
-    |> Enum.map(fn {key, val} ->
-      val
+    "input-large.txt"
+    |> parse_input()
+    |> Enum.reduce(%{}, fn cmd, acc -> do_cmd(cmd, acc) end)
+    |> Enum.map(fn {key, list} ->
+      list
       |> Enum.reverse()
       |> Enum.with_index(1)
       |> Enum.map(fn {{_, n}, i} -> (key + 1) * i * String.to_integer(n) end)
       |> Enum.sum()
     end)
     |> Enum.sum()
-
   end
 
   def do_cmd(cmd, map \\ %{}) do
@@ -33,15 +24,14 @@ defmodule Main do
     key_hash = hash(key)
     val = {key, n}
     list = map[key_hash]
+
     if list do
       i = Enum.find_index(list, fn {a, _b} -> a == key end)
+
       if i do
-        list = List.replace_at(list, i, val)
-        Map.put(map, key_hash, list)
+        Map.put(map, key_hash, List.replace_at(list, i, val))
       else
-        #list = list ++ [val]
-        list = [val | list]
-        Map.put(map, key_hash, list)
+        Map.put(map, key_hash, [val | list])
       end
     else
       Map.put(map, key_hash, [val])
@@ -51,6 +41,7 @@ defmodule Main do
   def do_cmd(key, :del, map) do
     key_hash = hash(key)
     list = map[key_hash]
+
     if list do
       list = Enum.reject(list, fn {a, _b} -> a == key end)
       Map.put(map, key_hash, list)
@@ -63,20 +54,10 @@ defmodule Main do
     s |> String.to_charlist() |> Enum.reduce(0, fn c, acc -> rem((acc + c) * 17, 256) end)
   end
 
-  def parse_input(path) do
-    path
-    |> File.read!()
-    |> String.trim()
-    |> String.split(",")
-  end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
-
+  def parse_input(path), do: path |> File.read!() |> String.trim() |> String.split(",")
 end
 
-Main.main() |> Main.p
+Main.main() |> IO.puts()
 
 # 145 - input-small.txt answer
 # 251353 - input-large.txt answer
