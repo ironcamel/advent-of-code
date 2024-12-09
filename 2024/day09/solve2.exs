685cdff1de92d74b358476e27b4428de2869fc7a
Naveed Massjouni <naveed@vt.edu>
2024-12-09 03:14:08 -0500

    refactor

--- 2024/day09/solve2.exs
+++ 2024/day09/solve2.exs
@@ -1,41 +1,35 @@
 defmodule Main do
   def main() do
-    #disk = "input-small.txt" |> parse_input()
-    disk = "input-large.txt" |> parse_input()
-
-    disk
-    |> fix()
+    "input-large.txt"
+    |> parse_input()
+    |> fix_disk()
     |> Enum.sort()
     |> Enum.flat_map(fn {_k, vals} -> vals end)
     |> Enum.with_index()
     |> Enum.map(fn
       {".", _i} -> 0
-      {val, i} -> i * val
+      {val, i} -> val * i
     end)
     |> Enum.sum()
   end
 
-  def fix(disk) do
+  def fix_disk(disk) do
     max_i = disk |> Map.keys() |> Enum.max()
 
     Enum.reduce(max_i..1//-1, disk, fn i, acc ->
-      orig_data = disk[i] |> Enum.reject(fn val -> val == "." end)
+      orig_data = Enum.reject(disk[i], fn val -> val == "." end)
       data = acc[i]
-
-      open_i = Enum.find(0..i-1, fn j ->
-        free_space(acc[j]) >= length(orig_data)
-      end)
+      open_i = Enum.find(0..(i - 1), fn j -> free_space(acc[j]) >= length(orig_data) end)
 
       if open_i do
         new_data = acc[open_i]
         free = free_space(new_data)
         new_data = Enum.reject(new_data, fn val -> val == "." end)
-        new_data = new_data ++ orig_data
-        new_data = new_data ++ List.duplicate(".", free - length(orig_data))
+        new_data = new_data ++ orig_data ++ List.duplicate(".", free - length(orig_data))
 
         acc
         |> Map.put(open_i, new_data)
-        |> Map.put(i, Enum.map(data, &(if &1 == i, do: ".", else: &1)))
+        |> Map.put(i, Enum.map(data, &if(&1 == i, do: ".", else: &1)))
       else
         acc
       end
