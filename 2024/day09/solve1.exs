877ec914cdf38fcba49c81472f91141becb1de7e
Naveed Massjouni <naveed@vt.edu>
2024-12-09 03:25:37 -0500

    refactor

--- 2024/day09/solve1.exs
+++ 2024/day09/solve1.exs
@@ -1,48 +1,20 @@
 defmodule Main do
   def main() do
-    # "input-large.txt"
-    #disk = "input-small.txt" |> parse_input()
-    disk = "input-large.txt" |> parse_input()
-    #num_dots = Enum.count(disk, fn c -> c == "." end)
-    final_size = Enum.count(disk, fn c -> c != "." end)
-    rev = disk |> Enum.reverse()
-    len = length(disk)
+    disk = parse_input("input-large.txt")
+    size = Enum.count(disk, fn c -> c != "." end)
 
-    fix(disk, rev, [], final_size)
-    |> dbg
+    disk
+    |> fix(Enum.reverse(disk), [], size)
     |> Enum.reject(fn val -> val == "." end)
     |> Enum.with_index()
-    |> Enum.map(fn {val, i} -> i * val end)
+    |> Enum.map(fn {val, i} -> val * i end)
     |> Enum.sum()
   end
 
-  def fix(_disk, _rev, fixed, final_size) when length(fixed) == final_size do
-    Enum.reverse(fixed)
-  end
-
-  def fix(disk, ["." | rev], fixed, final_size) do
-    #dbg()
-    fix(disk, rev, fixed, final_size)
-  end
-
-  def fix(["." | disk], [tail | rev], fixed, final_size) do
-    #dbg()
-    fix(disk, rev, [tail | fixed], final_size)
-  end
-
-  def fix([val | disk], rev, fixed, final_size) do
-    #dbg()
-    fix(disk, rev, [val | fixed], final_size)
-  end
-
-  def is_fixed(disk, rev, fixed) do
-    length(disk) + length(rev) == length(fixed)
-  end
-
-  def is_fixed2(disk) do
-    s = disk |> Enum.join()
-    s =~ ~r/^\d*\.*$/
-  end
+  def fix(_, _, fixed, size) when length(fixed) == size, do: Enum.reverse(fixed)
+  def fix(disk, ["." | rev], fixed, size), do: fix(disk, rev, fixed, size)
+  def fix(["." | disk], [tail | rev], fixed, size), do: fix(disk, rev, [tail | fixed], size)
+  def fix([val | disk], rev, fixed, size), do: fix(disk, rev, [val | fixed], size)
 
   def parse_input(path) do
     path
@@ -57,13 +29,9 @@ defmodule Main do
       {[a], i} -> List.duplicate(i, a)
     end)
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
 
 # 1928 - input-small.txt answer
 # 6310675819476 - input-large.txt answer
