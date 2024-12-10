f7850f3e8c4ce14826bf7aae9e40deb3d7602399
Naveed Massjouni <naveed@vt.edu>
2024-12-10 03:00:17 -0500

    refactor

--- 2024/day10/solve2.exs
+++ 2024/day10/solve2.exs
@@ -2,55 +2,26 @@ defmodule Main do
   @unit_circle [{-1, 0}, {0, 1}, {1, 0}, {0, -1}]
 
   def main() do
-    map = "input-large.txt" |> parse_input()
-    #map = "input-small.txt" |> parse_input()
-    #map = "foo.txt" |> parse_input()
-    #dbg map
+    map = parse_input("input-large.txt")
 
     map
-    |> Enum.filter(fn {_k, v} -> v == 0 end)
-    |> Enum.map(fn {p, _v} -> search(map, [p]) end)
-    |> Enum.map(fn paths -> length(paths) end)
+    |> Enum.filter(fn {_p, v} -> v == 0 end)
+    |> Enum.map(fn {p, _v} -> search(map, [p], 0) end)
     |> Enum.sum()
   end
 
-  def search(map, points, path \\ [], paths \\ [])
+  def search(_map, [], paths), do: paths
 
-  def search(_map, [], _path, paths) do
-    paths
-  end
-
-  def search(map, [point | points], path, paths) do
+  def search(map, [point | points], paths) do
     val = map[point]
-    path = [point | path]
 
     if val == 9 do
-      paths = [backtrack(map, path) | paths]
-      search(map, points, path, paths)
+      search(map, points, paths + 1)
     else
       @unit_circle
       |> Enum.map(fn dir -> add_points(point, dir) end)
       |> Enum.filter(fn p -> map[p] && map[p] == val + 1 end)
-      #|> Enum.map(fn p -> search(map, p, path) end)
-      |> then(fn next_points -> search(map, next_points ++ points, path, paths) end)
-    end
-  end
-
-  def backtrack(map, path, result \\ MapSet.new(), prev_val \\ 10)
-
-  def backtrack(_map, [], result, _prev_val) do
-    result
-  end
-
-  def backtrack(map, [point | path], result, prev_val) do
-    val = map[point]
-    #dbg()
-    cond do
-      val + 1 == prev_val ->
-        result = MapSet.put(result, point)
-        backtrack(map, path, result, val)
-      true ->
-        backtrack(map, path, result, prev_val)
+      |> then(fn next_points -> search(map, next_points ++ points, paths) end)
     end
   end
 
@@ -59,7 +30,6 @@ defmodule Main do
   def parse_input(path) do
     path
     |> File.read!()
-    |> String.trim()
     |> String.split("\n", trim: true)
     |> Enum.with_index()
     |> Enum.flat_map(fn {line, i} ->
@@ -70,13 +40,9 @@ defmodule Main do
     end)
     |> Map.new()
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
 
 # 81 - input-small.txt answer
 # 1086 - input-large.txt answer
