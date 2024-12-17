42edb0784ec608dffcf2271e9c132fd3bb018b0a
Naveed Massjouni <naveed@vt.edu>
2024-12-17 04:24:10 -0500

    day16 part 1

---
+++ 2024/day16/solve1.exs
@@ -0,0 +1,101 @@
+Mix.install([:heap])
+
+defmodule Main do
+  @north {-1, 0}
+  @east {0, 1}
+  @south {1, 0}
+  @west {0, -1}
+  @left_of %{@north => @west, @south => @east, @east => @north, @west => @south}
+  @right_of %{@north => @east, @south => @west, @east => @south, @west => @north}
+
+  def main() do
+    #grid = parse_input("input-small.txt")
+    #grid = parse_input("input-small2.txt")
+    grid = parse_input("input-large.txt")
+    {start, _} = Enum.find(grid, fn {_k, v} -> v == "S" end)
+    {target, _} = Enum.find(grid, fn {_k, v} -> v == "E" end)
+
+    dijkstra(grid, start, target)
+  end
+
+  def dijkstra(grid, start, target) do
+    start_node = {start, @east}
+    dist = %{start_node => 0}
+    #prev = %{}
+    visited = MapSet.new()
+    nodes = Heap.new() |> Heap.push({0, start_node})
+    dijkstra(grid, nodes, visited, dist, target)
+  end
+
+  def dijkstra(grid, nodes, visited, dist, target) do
+    {score, {pos, dir} = node} = Heap.root(nodes)
+    if pos == target do
+      score
+    else
+      nodes = Heap.pop(nodes)
+      visited = MapSet.put(visited, node)
+      straight_pos = add_points(pos, dir)
+
+      neighbors =
+        [
+          {{straight_pos, dir}, 1},
+          {{pos, @left_of[dir]}, 1000},
+          {{pos, @right_of[dir]}, 1000},
+        ]
+        |> Enum.reject(fn {{pos, _dir} = node, _cost} ->
+          MapSet.member?(visited, node) or grid[pos] == "#"
+        end)
+
+      dist = update_dist(node, dist, neighbors)
+
+      nodes =
+        neighbors
+        |> Enum.map(fn {node, _cost} -> node end)
+        |> Enum.reduce(nodes, fn node, acc ->
+          Heap.push(acc, {dist[node], node})
+        end)
+
+      dijkstra(grid, nodes, visited, dist, target)
+    end
+  end
+
+  def update_dist(node1, dist, neighbors) do
+    Enum.reduce(neighbors, dist, fn {node2, cost}, acc ->
+      val = acc[node1] + cost
+
+      if !acc[node2] or val < acc[node2] do
+        Map.put(acc, node2, val)
+      else
+        acc
+      end
+    end)
+  end
+
+  def add_points({i1, j1}, {i2, j2}), do: {i1 + i2, j1 + j2}
+
+  def parse_input(path) do
+    path
+    |> File.read!()
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, i} ->
+      line
+      |> String.codepoints()
+      |> Enum.with_index()
+      |> Enum.reject(fn {val, _j} -> val == "." end)
+      |> Enum.map(fn {val, j} -> {{i, j}, val} end)
+    end)
+    |> Map.new()
+  end
+
+  def p(o, opts \\ []) do
+    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
+  end
+end
+
+Main.main() |> Main.p()
+
+# 7036 - input-small.txt answer
+# 11048 - input-small2.txt answer
+# 79404 - input-large.txt answer
