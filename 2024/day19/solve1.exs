dee4e7f565bf0159ed955247e8a9a9bebc48902b
Naveed Massjouni <naveed@vt.edu>
2024-12-19 02:51:41 -0500

    refactor

--- 2024/day19/solve1.exs
+++ 2024/day19/solve1.exs
@@ -1,16 +1,10 @@
 defmodule Main do
   def main() do
-    #{patterns, designs} = parse_input("input-small.txt")
     {patterns, designs} = parse_input("input-large.txt")
-
-    designs
-    |> Enum.count(fn design ->
-      match(patterns, design)
-    end)
-
+    Enum.count(designs, fn design -> match(patterns, design) end)
   end
 
-  def match(patterns, ""), do: true
+  def match(_patterns, ""), do: true
 
   def match(patterns, design) do
     Enum.filter(patterns, fn p -> String.starts_with?(design, p) end)
@@ -20,20 +14,14 @@ defmodule Main do
   end
 
   def parse_input(path) do
-    [part1, part2] =
-      path
-      |> File.read!()
-      |> String.trim()
-      |> String.split("\n\n")
-
+    [part1, part2] = path |> File.read!() |> String.split("\n\n")
     patterns = String.split(part1, ", ")
     designs = String.split(part2, "\n", trim: true)
     {patterns, designs}
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
+
+# 6 - input-small.txt answer
+# 213 - input-large.txt answer
--- 2024/day19/solve2.exs
+++ 2024/day19/solve2.exs
@@ -2,24 +2,13 @@ Mix.install([:memoize])
 
 defmodule Main do
   use Memoize
-  def main() do
-    {patterns, designs} = parse_input("input-small.txt")
-    #{patterns, designs} = parse_input("input-large.txt")
 
-    match(patterns, "rrbgbr")
-    designs
-    |> Enum.map(fn design ->
-      match(patterns, design)
-    end)
-    |> Enum.sum()
+  def main() do
+    {patterns, designs} = parse_input("input-large.txt")
+    designs |> Enum.map(fn design -> match(patterns, design) end) |> Enum.sum()
   end
 
-    #if cache[design] do
-    #  [cache[design]]
-    #else
-    #end
-
-  defmemo match(_patterns, ""), do: 1
+  defmemo(match(_patterns, ""), do: 1)
 
   defmemo match(patterns, design) do
     patterns
@@ -31,23 +20,14 @@ defmodule Main do
   end
 
   def parse_input(path) do
-    [part1, part2] =
-      path
-      |> File.read!()
-      |> String.trim()
-      |> String.split("\n\n")
-
+    [part1, part2] = path |> File.read!() |> String.split("\n\n")
     patterns = String.split(part1, ", ")
     designs = String.split(part2, "\n", trim: true)
     {patterns, designs}
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
 
 # 16 - input-small.txt answer
 # 1016700771200474 - input-large.txt answer
