ea219457a1bb7f4531e205bafb6a356ed96b3114
Naveed Massjouni <naveed@vt.edu>
2024-12-21 07:36:20 -0500

    refactor

--- 2024/day21/solve1.exs
+++ 2024/day21/solve1.exs
@@ -5,69 +5,49 @@ defmodule Main do
   @west {0, -1}
 
   def main() do
-    #codes = parse_input("input-small.txt")
-    #codes = parse_input("foo.txt")
-    codes = parse_input("input-large.txt")
-
-    codes
+    "input-large.txt"
+    |> parse_input()
     |> Enum.map(fn code ->
-      l =
-        code
-        |> enter_code()
-        |> Enum.map(&Tuple.to_list/1)
-        |> Enum.map(fn code -> enter_code(code) end)
-        |> List.flatten()
-        |> Enum.map(&Tuple.to_list/1)
-        |> Enum.map(fn code -> enter_code(code, false) end)
-        |> List.flatten()
-        |> Enum.map(&Tuple.to_list/1)
-        |> Enum.map(&length/1)
-        |> Enum.min()
-      
       n = code |> Enum.filter(fn c -> c =~ ~r/\d/ end) |> Enum.join() |> String.to_integer()
-      n * l
-    end)
-    |> Enum.sum()
-    #42
-    #permutations(1..10 |> Enum.to_list) |> length
-    #permutations(List.duplicate(">", 5) ++ List.duplicate("^", 5)) |> length
-    #permutations(List.duplicate(">", 4) ++ List.duplicate("^", 3)) |> length
-    #permutations(List.duplicate(">", 4) ++ List.duplicate("^", 3)) |> Enum.uniq() |> length
-  end
 
-  def cost(code) do
-    buttons =
       code
       |> enter_code()
-      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(fn code -> enter_code(code) end)
+      |> List.flatten()
+      |> Enum.map(fn code -> enter_code(code) end)
+      |> List.flatten()
+      |> Enum.map(&tuple_size/1)
+      |> Enum.min()
+      |> then(fn min -> min * n end)
+    end)
+    |> Enum.sum()
   end
 
-  def enter_code(code, gen_perm \\ true, path \\ [], cur \\ {3, 2})
+  def enter_code(code, path \\ [], cur \\ {3, 2})
+  def enter_code([], path, _cur), do: List.to_tuple(path)
 
-  def enter_code([], _gen_perm, path, _cur), do: path |> List.to_tuple()
+  def enter_code(code, path, cur) when is_tuple(code) do
+    enter_code(Tuple.to_list(code), path, cur)
+  end
 
-  def enter_code([c | code], gen_perm, path, {i0, j0} = cur) do
-    #dbg()
-    #p path
+  def enter_code([c | code], path, {i0, j0} = cur) do
     {i1, j1} = new_pos = pos_for(c)
-    #dist = taxi_dist(cur, new_pos)
     vert_dist = i1 - i0
     horz_dist = j1 - j0
     vert_c = if vert_dist > 0, do: "v", else: "^"
     horz_c = if horz_dist > 0, do: ">", else: "<"
 
-
-    permutations(abs(vert_dist), vert_c, abs(horz_dist), horz_c)
+    gen_buttons(abs(vert_dist), vert_c, abs(horz_dist), horz_c)
     |> Enum.reject(fn buttons ->
       buttons
-      |> Enum.reduce([cur], fn arrow, acc->
+      |> Enum.reduce([cur], fn arrow, acc ->
         new_pos = arrow |> arrow_to_dir() |> add_points(hd(acc))
         [new_pos | acc]
       end)
       |> Enum.any?(fn p -> p == {3, 0} end)
     end)
     |> Enum.map(fn buttons ->
-      enter_code(code, gen_perm, path ++ buttons ++ ["A"], new_pos)
+      enter_code(code, path ++ buttons ++ ["A"], new_pos)
     end)
     |> List.flatten()
   end
@@ -81,10 +61,10 @@ defmodule Main do
     end
   end
 
-  def permutations(x, c1, y, c2) do
+  def gen_buttons(n1, c1, n2, c2) do
     [
-      List.duplicate(c1, x) ++ List.duplicate(c2, y),
-      List.duplicate(c2, y) ++ List.duplicate(c1, x),
+      List.duplicate(c1, n1) ++ List.duplicate(c2, n2),
+      List.duplicate(c2, n2) ++ List.duplicate(c1, n1)
     ]
     |> Enum.uniq()
   end
@@ -112,11 +92,7 @@ defmodule Main do
   def add_points({i1, j1}, {i2, j2}), do: {i1 + i2, j1 + j2}
 
   def parse_input(path) do
-    path
-    |> File.read!()
-    |> String.split("\n", trim: true)
-    |> Enum.map(fn line -> line |> String.codepoints()
-    end)
+    path |> File.read!() |> String.split("\n", trim: true) |> Enum.map(&String.codepoints/1)
   end
 
   def p(o, opts \\ []) do
@@ -124,7 +100,7 @@ defmodule Main do
   end
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
 
 # 126384 - input-small.txt answer
 # 137870 - input-large.txt answer
