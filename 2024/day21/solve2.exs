b5451845b5c44234b49bd2f203533f08073f7597
Naveed Massjouni <naveed@vt.edu>
2024-12-22 06:51:50 -0500

    refactor

--- 2024/day21/solve2.exs
+++ 2024/day21/solve2.exs
@@ -4,35 +4,12 @@ defmodule Main do
   use Memoize
 
   def main() do
-    depth = 2
-    depth = 25
-    dbg(depth)
-
     "input-large.txt"
     |> parse_input()
     |> Enum.map(fn code ->
-      dbg code
       n = code |> Enum.join() |> String.replace("A", "") |> String.to_integer()
-      #code = encode(code)
-
-      #1..depth
-      #|> Enum.reduce(code, fn level, acc ->
-      #  p level: level, length: length(acc)
-      #  encode(acc)
-      #end)
-      #|> then(fn code -> length(code) * n end)
-
-      l = calc_len(code, depth)
-      n * l
+      calc_len(code, 25) * n
     end)
-    |> Enum.sum()
-  end
-
-  defmemo calc_len(code, 0) do
-    #dbg()
-    ["A" | code]
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.map(fn [a, b] -> a |> path_for(b) |> length() end)
     |> Enum.sum()
   end
 
@@ -40,15 +17,12 @@ defmodule Main do
     ["A" | code]
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.map(fn [a, b] ->
-      calc_len(path_for(a, b), depth - 1)
+      case depth do
+        0 -> path_for(a, b) |> length()
+        depth -> calc_len(path_for(a, b), depth - 1)
+      end
     end)
     |> Enum.sum()
-  end
-
-  def encode(code) do
-    ["A" | code]
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.flat_map(fn [a, b] -> path_for(a, b) end)
   end
 
   def path_for("A", "<"), do: ~w( v < < A )
@@ -80,13 +54,9 @@ defmodule Main do
     horz_dist = abs(horz_dist)
 
     # prefer < v ^ >   
-    cond do
-      horz_dist != 0 and vert_dist == 0 -> List.duplicate(horz_c, horz_dist)
-      horz_dist == 0 and vert_dist != 0 -> List.duplicate(vert_c, vert_dist)
-      horz_c == "<" -> List.duplicate(horz_c, horz_dist) ++ List.duplicate(vert_c, vert_dist)
-      vert_c == "v" -> List.duplicate(vert_c, vert_dist) ++ List.duplicate(horz_c, horz_dist)
-      vert_c == "^" -> List.duplicate(vert_c, vert_dist) ++ List.duplicate(horz_c, horz_dist)
-      horz_c == ">" -> List.duplicate(vert_c, vert_dist) ++ List.duplicate(horz_c, horz_dist)
+    case horz_c do
+      "<" -> List.duplicate(horz_c, horz_dist) ++ List.duplicate(vert_c, vert_dist)
+      _ -> List.duplicate(vert_c, vert_dist) ++ List.duplicate(horz_c, horz_dist)
     end
     |> then(fn path -> path ++ ["A"] end)
   end
@@ -108,21 +78,14 @@ defmodule Main do
       "<" -> {4, 0}
       "v" -> {4, 1}
       ">" -> {4, 2}
-      " " -> {3, 0}
     end
   end
-
-  def add_points({i1, j1}, {i2, j2}), do: {i1 + i2, j1 + j2}
 
   def parse_input(path) do
     path |> File.read!() |> String.split("\n", trim: true) |> Enum.map(&String.codepoints/1)
   end
-
-  def p(o, opts \\ []) do
-    IO.inspect(o, [charlists: :as_lists, limit: :infinity] ++ opts)
-  end
 end
 
-Main.main() |> Main.p()
+Main.main() |> IO.puts()
 
 # 170279148659464 - input-large.txt answer
