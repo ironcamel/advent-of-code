01b498171fff7b5e109f5a152b47e8c0fc4493e0
Naveed Massjouni <naveed@vt.edu>
2025-12-06 02:27:43 -0500

    refactor

--- 2025/day06/solve2.exs
+++ 2025/day06/solve2.exs
@@ -1,32 +1,21 @@
-defmodule Main do
-  def main() do
-    {lines, ops} = parse_input("input-large.txt")
+lines = "input-large.txt" |> File.read!() |> String.split("\n", trim: true)
+{lines, [ops]} = Enum.split(lines, -1)
+ops = String.split(ops)
 
-    lines
-    |> Enum.map(&String.codepoints/1)
-    |> Enum.zip()
-    |> Enum.map(fn chars ->
-      chars |> Tuple.to_list() |> Enum.join() |> String.trim()
-    end)
-    |> Enum.chunk_by(fn s -> s == "" end)
-    |> Enum.reject(&(&1 == [""]))
-    |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
-    |> Enum.zip(ops)
-    |> Enum.map(fn
-      {vals, "+"} -> Enum.sum(vals)
-      {vals, "*"} -> Enum.product(vals)
-    end)
-    |> Enum.sum()
-  end
-
-  def parse_input(path) do
-    lines = path |> File.read!() |> String.split("\n", trim: true)
-    {lines, [ops]} = Enum.split(lines, -1)
-    {lines, String.split(ops)}
-  end
-end
-
-Main.main() |> IO.puts()
+lines
+|> Enum.map(&String.codepoints/1)
+|> Enum.zip()
+|> Enum.map(fn digits -> digits |> Tuple.to_list() |> Enum.join() |> String.trim() end)
+|> Enum.chunk_by(fn s -> s == "" end)
+|> Enum.reject(&(&1 == [""]))
+|> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+|> Enum.zip(ops)
+|> Enum.map(fn
+  {vals, "+"} -> Enum.sum(vals)
+  {vals, "*"} -> Enum.product(vals)
+end)
+|> Enum.sum()
+|> IO.puts()
 
 # 3263827 - input-small.txt answer
 # 11386445308378 - input-large.txt answer
